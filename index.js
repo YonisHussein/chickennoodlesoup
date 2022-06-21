6b207ab4f6cae8a79d1db6635393075fa6a75b0b
YonisHussein <yonismhussein@gmail.com>
2022-06-21 19:15:31 -0400

    Added start employee

--- index.js
+++ index.js
@@ -10,3 +10,58 @@ const db = mysql.createConnection(
         database: null,
     },
 );
+db.connect(function(err){
+    if (err) throw err;
+    console.log("Connected as id'+ db.threadID");
+})
+
+function startEmplotee(){
+    inquirer.prompt({
+        type:'list',
+        name:'options',
+        message:'What would you like to do?',
+        choice:[
+            'View All Departments',
+            'View All Roles',
+            'View All Employees',
+            'Add Department',
+            'Add Role',
+            'Add Emplotee',
+            'Exit'
+        ]
+    })
+    .then((res) => {
+        console.log(res.options);
+        switch(res.options){
+            case 'View All Departments':
+                viewAllDepartments();
+                break;
+            case "View All Roles":
+                viewAllRoles();
+                break;
+            case 'View All Emplotees':
+                viewAllEmployees();
+                break
+            case 'Add Department':
+                addDepartment();
+                break;
+            case 'Update Employee Role':
+                updateEmploteeRole();
+                break;
+            case 'Exit':
+                db.end();
+                break;
+
+        }
+    });
+}
+
+function viewAllDepartments(){
+    let query = "SELECT * FROM department";
+    db.query(query, function(err,res){
+        if(err) throw err;
+        console.table
+    })
+}
+
+
