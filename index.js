c7fdf2194ba792151e4b9a1fe78bd0d67e0a03cf
YonisHussein <yonismhussein@gmail.com>
2022-06-21 18:53:54 -0400

    Initial Commit

---
+++ index.js
@@ -0,0 +1,12 @@
+const inquirer = require('inquirer');
+const mysql = require('mysql2');
+const cTable = require('console.table');
+
+const db = mysql.createConnection(
+    {
+        host: 'localhost',
+        user: 'root',
+        password: 'minnasota',
+        database: null,
+    },
+);
