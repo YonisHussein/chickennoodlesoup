7caaee4ce6a65bf0d8bd2066b039c8c8c57d2e19
YonisHussein <yonismhussein@gmail.com>
2022-06-21 21:05:22 -0400

    schema

---
+++ schema.sql
@@ -0,0 +1,18 @@
+DROP DATABASE IF EXISTS employee_db;
+CREATE DATABASE employee_db;
+
+USE employee_db;
+
+CREATE TABLE department(
+    id INT NOT NULL AUTO_INCREMENT,
+    name VARCHAT(30) NULL,
+    PRIMARY KEY (id)
+);
+
+CREATE TABLE role(
+    id INT NOT NULL AUTO_INCREMENT,
+    title VARCHAR(30) NULL,
+    salary DECIMAL(10.2) NULL,
+    department_id INT NULL,
+    PRIMARY KEY (id)
+);
