745927171c791cde5b2cd8a798bef9b6eb4356e2
YonisHussein <yonismhussein@gmail.com>
2022-06-21 20:26:42 -0400

    index.js inbound

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ db.connect(function(err){
     console.log("Connected as id'+ db.threadID");
 })
 
-function startEmplotee(){
+function startEmployee(){
     inquirer.prompt({
         type:'list',
         name:'options',
@@ -60,8 +60,43 @@ function viewAllDepartments(){
     let query = "SELECT * FROM department";
     db.query(query, function(err,res){
         if(err) throw err;
-        console.table
+        console.table(res);
+        startEmployee();
     })
 }
 
+function addDepartment(){
+    inquirer.prompt({
+        type:'input',
+        name:'department',
+        message:'Please enter the department you want to add.'
+    })
+    .then((res) => {
+        let query = 'INSERT INTO department SET?';
+        db.query(query, {department:res.department},(err,res) => {
+            if(err) throw err;
+            startEmployee();
+        });
+    });
+}
+
+function addRole(){
+    inquirer.prompt([
+        {
+            type:'input',
+            name:'role',
+            message:'Please enter the role you want to add.'
+        },
+        {
+            type:'number',
+            name:'salary',
+            message:'Please enter the salary of the role.'
+        },
+        {
+            type:'input',
+            name:'dpartment',
+            message:''
+        }
+    ]);
+}
 
